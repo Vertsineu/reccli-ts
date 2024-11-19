cee976f38d91cdfcef5025c91ed874f4274ccdf7
niuzai1000 <wuqingyu05@mail.ustc.edu.cn>
2024-11-19 16:35:30 +0800

    Implement some apis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript!");
+import RecAPI from "./RecAPI";
+
+const api = new RecAPI();
+
+async function main() {
+    await api.login("Username", "Password");
+    const userInfo = await api.getUserInfo();
+    console.log(userInfo);
+}
+
+main().catch(console.error);
