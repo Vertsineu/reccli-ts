b7484c7a10d14b1990f4a281187c5631926e8a7c
niuzai1000 <wuqingyu05@mail.ustc.edu.cn>
2024-11-25 20:23:46 +0800

    store UserAuth with hashed file name

--- src/utils/rec-user-auth-saver.ts
+++ src/utils/rec-user-auth-saver.ts
@@ -1,12 +1,17 @@
 import fs from "fs"
+import crypto from 'crypto';
 import { homedir } from "os";
 import { UserAuth } from "./rec-api"
 
 const dirPath = `${homedir()}/.reccli-ts`;
 
+function getFileName(account: string): string {
+    return crypto.createHash('sha256').update(account).digest('hex');
+}
+
 // read user auth from file "~/.reccli-ts"
 function getUserAuth(account: string): UserAuth | undefined {
-    const path = `${dirPath}/${account}.json`;
+    const path = `${dirPath}/${getFileName(account)}`;
     if (!fs.existsSync(path)) return undefined;
     const userAuth = JSON.parse(fs.readFileSync(path, 'utf8'));
     return userAuth;
@@ -14,12 +19,14 @@ function getUserAuth(account: string): UserAuth | undefined {
 
 function setUserAuth(account: string, userAuth: UserAuth) {
     if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
-    const path = `${dirPath}/${account}.json`;
+    const path = `${dirPath}/${getFileName(account)}`;
+    // don't save information that is not necessary
+    userAuth.gid = ""; userAuth.name = ""; userAuth.username = "";
     fs.writeFileSync(path, JSON.stringify(userAuth));
 }
 
 function deleteUserAuth(account: string): boolean {
-    const path = `${dirPath}/${account}.json`;
+    const path = `${dirPath}/${getFileName(account)}`;
     if (!fs.existsSync(path)) return false;
     fs.unlinkSync(path);
     return true;
